54e61f3338d911e692ca320c565498f077227474
Damelia Coleman <dameliac@outlook.com>
2025-06-24 03:32:06 -0500

    imported chess library

---
+++ 620158703_chessAI.py
@@ -0,0 +1,12 @@
+#import required chess module
+import chess
+import chess.engine
+
+
+#create and display chess board
+board = chess.Board()
+
+def display_board(board):
+    print(board)
+
+display_board(board)
